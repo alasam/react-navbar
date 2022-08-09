7e5e620f6c743e4d048c92bcdfcd466cdc5b5da8
Anthony <alasam@live.com>
2022-08-09 01:45:53 -0400

    Clean app.css, add fontawesome kit, add additional content to Navbar.js

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+const MainItems = [
+  {
+    label: 'Services',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    label: 'Products',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    label: 'Cpmtact Is',
+    url: '#',
+    cName: 'nav-links'
+  },
+  {
+    label: 'Sign up',
+    url: '#',
+    cName: 'nav-links'
+  }
+]
